e66590e47bcb63d04bf57a015625bab774cff1b6
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-01 16:13:30 +0100

    populated Navbar element. Installed React Icons.

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import './Navbar.css'
+import data from './data'
+import {IoAccessibility} from 'react-icons/io5'
 
 const Navbar = () => {
   return (
-    <nav id="navbar"><h2>Navbar</h2></nav>
+    <nav id="navbar">
+      <div className="container nav__container">
+      <a href="index.html" className="nav__logo">
+        Natalie Gillam - React Developer
+      </a>
+      <ul className='nav__menu'>
+        {data.map(item=><li key={item.id}><a href={item.link}>{item.title}</a></li>)}
+      </ul>
+      <button id="accessibility-options"><IoAccessibility/> </button>
+      </div>
+    </nav>
   )
 }
 
---
+++ src/sections/navbar/data.js
@@ -0,0 +1,9 @@
+const data = [
+    {id: 1, link: '#', title: 'Home'}, 
+    {id: 2, link: '#skills', title: 'Skills'},
+    {id: 3, link: '#portfolio', title: 'Portfolio'},
+    {id: 4, link: '#about', title: 'About'},
+    {id: 5, link: '#contact', title: 'Contact'}
+]
+
+export default data
