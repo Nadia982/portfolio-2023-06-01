edd880292d2b8d01460cc6d90a4dfc28834ad377
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-02 00:01:21 +0100

    added card component to About section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,3 +1,5 @@
+import data from './data'
+import Card from '../../components/Card'
 import './About.css'
 
 const About = () => {
@@ -5,14 +7,25 @@ const About = () => {
     <section id="about">
       <div className="container about__container">
         <h2>About Me</h2>
-        <div className="about__cards"></div>
+        <div className="about__cards">
+          {
+            data.map(item => (
+              <Card key={item.id} className="about__card" >
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+
+              </Card>
+            ))
+          }
+        </div>
         {/* <div className="about__left"> */}
         <p>I have worked in the tech space for the past 11 years, most recently as a senior paralegal at a global IT services company headquartered in London. However, my real passion and new professional focus is <strong>frontend web development</strong>.</p>
         {/* </div>
         <div className="about__right"> */}
           <p>I am an avid learner and am keen to learn as much as possible about new and existing technologies while making websites and applications that delight and engage users.</p>
         {/* </div> */}
-        
+
       </div>
     </section>
   )
---
+++ src/sections/about/data.js
@@ -0,0 +1,11 @@
+import {FaAward} from 'react-icons/fa'
+import {FaBook} from 'react-icons/fa'
+import {CgWebsite} from 'react-icons/cg'
+
+const data = [
+    {id: 1, icon: <FaAward/>, title: 'Experience', desc: '1 year of experience'},
+    {id: 2, icon: <FaBook/>, title: 'Certifications', desc: '2000+ hours of study'},
+    {id: 3, icon: <CgWebsite/>, title: 'Projects', desc: '10+ projects completed'},
+]
+
+export default data;
