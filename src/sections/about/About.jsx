28dfb3438d2dfc0850e2cb5df4efc198b6076878
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-01 23:00:12 +0100

    added content of about section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -2,7 +2,19 @@ import './About.css'
 
 const About = () => {
   return (
-    <section id="about"><h2>About</h2></section>
+    <section id="about">
+      <div className="container about__container">
+        <h2>About Me</h2>
+        <div className="about__cards"></div>
+        {/* <div className="about__left"> */}
+        <p>I have worked in the tech space for the past 11 years, most recently as a senior paralegal at a global IT services company headquartered in London. However, my real passion and new professional focus is <strong>frontend web development</strong>.</p>
+        {/* </div>
+        <div className="about__right"> */}
+          <p>I am an avid learner and am keen to learn as much as possible about new and existing technologies while making websites and applications that delight and engage users.</p>
+        {/* </div> */}
+
+      </div>
+    </section>
   )
 }
 
--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -13,7 +13,6 @@ const Header = () => {
           
         </div>
         <p>I am based in <strong>London, UK</strong> and have been building websites with React and JavaScript since 2022. As at June 2023, I am <strong>open</strong> to new work opportunities. If you would like to get in touch then please:</p> 
-      {/* <p>I have worked in the tech space for the past 11 years, most recently as a senior paralegal at a global IT services company headquartered in London. However, my real passion and new professional focus is frontend web development.</p> */}
       
       <div className="header__cta">   
       <a href="https://www.linkedin.com/in/natalie-g-b947331a/" className="btn primary">Message me on LinkedIn</a>
