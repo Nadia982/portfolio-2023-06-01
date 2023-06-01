798b40d759df3a17b84a0cb2944afdc9783668ee
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-01 21:40:02 +0100

    added header content

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,31 @@
+import HeaderImage from '../../assets/natalie.jpg'
+import data from './data'
 import './Header.css'
 
 const Header = () => {
   return (
-    <section id="header"><h2>Header</h2></section>
+    <section id="header">
+      <div className="container header__container">
+        <div className="header__profile">
+          
+          <img src={HeaderImage} alt="photo of Natalie Gillam" />
+          <h3>Natalie Gillam</h3>
+          
+        </div>
+        <p>I am based in <strong>London, UK</strong> and have been building websites with React and JavaScript since 2022.</p> 
+      <p>As at June 2023, I am <strong>open</strong> to new work opportunities. </p>
+      {/* <p>I have worked in the tech space for the past 11 years, most recently as a senior paralegal at a global IT services company headquartered in London. However, my real passion and new professional focus is frontend web development.</p> */}
+      <p>If you would like to get in touch then please:</p> 
+      <div className="header__cta">   
+      <a href="https://www.linkedin.com/in/natalie-g-b947331a/" className="btn primary">Message me on LinkedIn</a>
+      </div>
+      <p>Click on the icons below to see my skills and experience in the following technologies:</p>
+      </div>
+
+      <div className="header__socials">
+        {data.map(item => <a key={data.id} href={data.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)}
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,9 @@
+import {AiOutlineGithub }  from 'react-icons/ai'
+import {AiOutlineLinkedin }  from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://www.linkedin.com/in/natalie-g-b947331a/}', icon: <AiOutlineLinkedin/>},
+    {id: 2, link: 'https://github.com/Nadia982}', icon: <AiOutlineGithub/>}
+]
+export default data;
+
