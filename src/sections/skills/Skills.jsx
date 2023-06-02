e2fe534bd74579f7041537253ae3a7dbe90406e2
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-02 13:58:40 +0100

    added content of skills section

--- src/sections/skills/Skills.jsx
+++ src/sections/skills/Skills.jsx
@@ -1,8 +1,25 @@
+import Card from '../../components/Card'
+import data from './data'
 import './Skills.css'
 
 const Skills = () => {
   return (
-    <section id="skills"><h2>Skills</h2></section>
+    <section id="skills">
+      {/* <h2>Skills</h2>
+      <p>I have skills in:</p> */}
+      <div className="container skills__container">
+        {
+          data.map(item=> (
+            <Card key={item.id} className="skill light">
+              <div className="skill__icon">{item.icon}</div>
+              <p>{item.title}</p>
+            </Card>
+
+          ) )
+        }
+
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/skills/data.js
@@ -0,0 +1,25 @@
+import {IoLogoHtml5} from 'react-icons/io5'
+import {IoLogoCss3} from 'react-icons/io5'
+import {FiFigma} from 'react-icons/fi'
+import {IoLogoJavascript} from 'react-icons/io5'
+import {IoLogoReact} from 'react-icons/io5'
+import {GiHook} from 'react-icons/gi'
+import {IoGitBranchOutline} from 'react-icons/io5'
+import {IoLogoGithub} from 'react-icons/io5'
+import {TbApi} from 'react-icons/tb'
+import {SiTypescript} from 'react-icons/si'
+
+const data = [
+    {id: 1, icon: <IoLogoHtml5/>, title: "HTML5"},
+    {id: 2, icon: <IoLogoCss3/>, title: "CSS3"},
+    {id: 3, icon: <FiFigma/>, title: "Figma"},
+    {id: 4, icon: <IoLogoJavascript/>, title: "JavaScript"},
+    {id: 5, icon: <IoLogoReact/>, title: "React 18"},
+    {id: 6, icon: <GiHook/>, title: "React Hooks"},
+    {id: 7, icon: <IoGitBranchOutline/>, title: "Git"},
+    {id: 8, icon: <IoLogoGithub/>, title: "GitHub"},
+    {id: 9, icon: <TbApi/>, title: "APIs"},
+    {id: 10, icon: <SiTypescript/>, title: "TypeScript"}
+]
+
+export default data
